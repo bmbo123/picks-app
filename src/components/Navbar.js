7651f23eb4c3c8bf3127ac73fcd407a2fbbf97fe
bmbo123 <gregoryalan@gmail.com>
2024-05-17 00:02:21 -0500

    did some work

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ function Navbar(){
         <nav className="bg-gray-800 p-4">
              <div className="container mx-auto">
                 <h1 className="text-white text-2xl font-sans">EzPicks</h1>
-      </div>
+            </div>
         </nav>
 
     )
--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -1,10 +1,12 @@
 import React from "react";
 import './SideNav.css';
+
 function SideNav(){
     return(
         <div className="side-main">
-
-            
+            <div className="platform-box">All Platforms</div>
+            <div className="platform-box">PrizePicks</div>
+            <div className="platform-box">UnderDog</div>
         </div>
 
     )
