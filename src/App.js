8039964deefd05c51cf3745cbb15927daf04ab19
bmbo123 <gregoryalan2005@gmail.com>
2024-06-10 20:15:59 -0500

    did something

--- src/App.js
+++ src/App.js
@@ -10,6 +10,20 @@ const url =
   "https://api.prizepicks.com/projections?league_id=7&per_page=250&single_stat=true&projection_type_id=1&sort=player_name";
 function App() {
   const [players, setPlayers] = useState([]);
+  const playerMap = new Map();
+  for (let i = 0; i < jsonData.included.length; i++) {
+    if (
+      jsonData.included[i].type === "new_player" ||
+      jsonData.included[i].type === "stat_type"
+    ) {
+      playerMap.set(jsonData.included[i].id, {
+        name: jsonData.included[i].attributes.name,
+        id: jsonData.included[i].attributes.image_url,
+        combo: jsonData.included[i].attributes.combo,
+      });
+    }
+  }
+  console.log(playerMap);
   useEffect(() => {
     setPlayers(jsonData.included);
   }, []);
